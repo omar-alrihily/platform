e01a9dadc7bb665fbc758c560b212cff10d4b03f
omar-alrihily <omar.8260@gmail.com>
2023-01-17 14:16:15 +0300

    navbar and footer
    first day

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import Menu from './menu' ;
+import Footer from './Footer'
+
+function App() {
+  return (
+    <div className="App bg-sky-100 min-h-screen">
+
+      
+
+      <Menu/>
+      <h1 className='bg-red-50	'>hiii</h1>
+
+      <Footer/>
+
+      
+
+     
+    </div>
+  );
+}
+
+export default App;
