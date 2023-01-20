cc8d7bd553cdb226dee8b6b8ccea92b41aedbc04
omar-alrihily <omar.8260@gmail.com>
2023-01-20 18:53:39 +0300

    convert to RTL
    arabic page

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
+  theme: {  fontFamily: {
+    'tajawal': ['tajawal', 'sans-serif'],
+    'MyFont': ['"My Font"', 'serif'] // Ensure fonts with spaces have " " surrounding it.
+  },
     extend: {},
   },
   plugins: [],
