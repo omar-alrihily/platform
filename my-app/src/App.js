241daef5c9de3dc38e68e8ef6ff2067fc1f14e29
omar-alrihily <omar.8260@gmail.com>
2023-01-20 16:38:36 +0300

    add intro

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,15 +2,16 @@
 import './App.css';
 import Menu from './menu' ;
 import Footer from './Footer'
+import Intro from './intro'
 
 function App() {
   return (
-    <div className="App bg-sky-100 min-h-screen">
+    <div className="App static bg-sky-100 min-h-screen  ">
 
       
 
       <Menu/>
-      <h1 className='bg-red-50	'>hiii</h1>
+      <Intro/>
 
       <Footer/>
 
--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div>
-        <footer class=" absolute bottom-0 left-0  w-6/12   object-bottomp-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+        <footer class=" absolute bottom-0  inset-x-4  object-bottomp-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
     <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
     </span>
     <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
